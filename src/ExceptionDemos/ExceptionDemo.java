b3a215b0e21e4b9605d2b2b39fcdd5bed85667f8
guanlong zhang <2759819042@qq.com>
2023-11-13 11:45:50 +0800

    add

---
+++ src/ExceptionDemos/ExceptionDemo.java
@@ -0,0 +1,12 @@
+package ExceptionDemos;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        method();
+    }
+
+    public static void method(){
+        int [] arr = {1, 2, 3};
+        System.out.println(arr[0]);
+    }
+}
